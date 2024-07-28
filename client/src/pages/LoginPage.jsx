ef1784c3039f427b83c1b6debdd37fbc048a9c3b
Leart23 <leartmusahu@gmail.com>
2024-07-28 19:15:04 +0200

    pages

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/no-unescaped-entities */
+import axios from "axios";
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  async function handleLoginSubmit(ev) {
+    ev.preventDefault();
+
+    try {
+      await axios.post("/login", { email, password });
+      alert("Login successful!");
+      setRedirect(true);
+    } catch (e) {
+      alert("Login failed!");
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-3xl text-center mb-4">Login</h1>
+        <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
+          <button className="primary">Login</button>
+          <div className="text-center text-gray-500 py-2">
+            Don't have an account yet?{" "}
+            <Link className="underline text-black" to={"/register"}>
+              Register now!
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
